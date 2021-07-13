984d37bbf923c5f8adb890383874a21513601db4
Ryomanba <datemanba3@gmail.com>
2021-07-13 20:24:25 +0900

    Add Problem 024

---
+++ Problems/027.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+	int	n;
+	cin >> n;
+	vector<int> ans;
+	map<string, int> mp;
+	rep(i,n)
+	{
+		string s;
+		cin >> s;
+		mp[s]++;
+		if (mp[s] == 1)
+			ans.push_back(i + 1);
+	}
+	for (auto x : ans)
+		cout << x << endl;
+	return 0;
+}
