4c76128b08c292a89ee850f9db4ec952d80fec71
Ryomanba <datemanba3@gmail.com>
2021-07-25 17:41:27 +0900

    Add Problem 020

---
+++ Problems/020.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+// log2a < blog2cかどうか。
+
+int main()
+{
+	ll a, b, c, tmp;
+	cin >> a >> b >> c;
+
+	tmp = c;
+	rep(i,b - 1)
+		tmp *= c;
+	if (a < tmp)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	return 0;
+}
