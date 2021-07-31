eba3ad93f136d893c6a06da05079faceb8a9e19d
Ryomanba <datemanba3@gmail.com>
2021-07-31 12:39:45 +0900

    dp

---
+++ Problems/050.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+ll dp[100100] = {};
+
+int main()
+{
+	ll n, l;
+	cin >> n >> l;
+
+	dp[0] = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i < l)
+			dp[i] = dp[i - 1];
+		else
+			dp[i] = (dp[i - 1] + dp[i - l]) % MOD; // lより大きくなったら, i - l から上がってくる場合も考慮する
+	}
+	cout << dp[n] << endl;
+	return 0;
+}
