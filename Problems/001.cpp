bb32733d7f361de23f67998c71a9db0f12d5c5e4
Ryomanba <datemanba3@gmail.com>
2021-08-11 23:26:20 +0900

    二分探索

--- Problems/001.cpp
+++ Problems/001.cpp
@@ -13,39 +13,44 @@ int dy[]={1, -1, 0, 0};
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
-ll n, l, k;
-vector<ll> a(10100);
+ll n, k, l;
+ll a[100100];
 
-bool solve(ll m)
+bool isOK(ll m)
 {
-    ll cnt = 0, pre = 0;
-    rep(i,n)
-    {
-        if (a[i] - pre >= m && l - a[i] >= m)
-        {
-            cnt++;
-            pre = a[i];
-        }
-    }
-    if (cnt >= k) return true;
-    return false;
+	ll cnt = 0, pre = 0;
+	rep(i,n)
+	{
+		if (a[i] - pre >= m && l - a[i] >= m)
+		{
+			cnt++;
+			pre = a[i];
+		}
+	}
+	if (cnt >= k) return true;
+	return false;
+
+}
+
+ll binary_search()
+{
+	ll left = -1;
+	ll right = l;
+
+	while (abs(right - left) > 1) {
+		ll mid = left + (right - left) / 2;
+		if (isOK(mid) == false) right = mid;
+		else left = mid;
+	}
+	return left;
 }
 
 int main()
 {
-    cin >> n >> l;
-    cin >> k;
-    rep(i,n) cin >> a[i];
+	cin >> n >> l >> k;
+	rep(i,n) cin >> a[i];
 
-    ll left = -1, right = l + 1;
-
-    // 最大で何個の長さ M 以上のピースに分割できるか二分探索
-    while (right - left > 1)
-    {
-        ll mid  = left + (right - left) / 2;
-        if (solve(mid) == false) right = mid;
-        else    left = mid;
-    }
-    cout << left << endl;
-    return 0;
+	ll ans = binary_search();
+	cout << ans << endl;
+	return 0;
 }
