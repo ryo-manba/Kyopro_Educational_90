1512287824c3272e9c5a95448d8699d1f605fa34
Ryomanba <datemanba3@gmail.com>
2021-08-12 22:58:31 +0900

    木の直径(BFS)

--- Problems/003.cpp
+++ Problems/003.cpp
@@ -13,72 +13,58 @@ int dy[]={1, -1, 0, 0};
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
-/*  木の直径は最短距離計算を2回する
-    1. 単純パスの長さの最大値を求める。
-    2. 頂点1から各頂点までの最短距離を求める。
-    3. 最も最短距離が大きかった頂点からの最短距離を求める。
-    4. その時に出現した最短距離の最大値が木の直径となる。
-*/
+int	n;
+int	a[100100], b[100100], dist[100100];
+vector<int> G[100100]; // グラフ
 
-int n;
-vector<vector<int>> G;
-vector<int> dist;
-
-// BFSで最短距離を計算
-void getdist(int start)
+void getdist(int start) // bfsで最短距離を求める
 {
-    dist.assign(n, INF);
-    queue<int> que;
-    que.push(start);
-    dist[start] = 0;
-
-    while (!que.empty())
-    {
-        int pos = que.front();
-        que.pop();
-        for (int to : G[pos])
-        {
-            if (dist[to] == INF)
-            {
-                dist[to] = dist[pos] + 1;
-                que.push(to);
-            }
-        }
-    }
+	for (int i = 1; i <= n; i++) dist[i] = INF;
+	queue<int> que;
+	que.push(start);
+	dist[start] = 0;
+	while (!que.empty())
+	{
+		int pos = que.front();
+		que.pop();
+		for (int to : G[pos])
+		{
+			if (dist[to] == INF)
+			{
+				dist[to] = dist[pos] + 1;
+				que.push(to);
+			}
+		}
+	}
 }
+
 
 int main()
 {
-    cin >> n;
-    G.resize(n + 1);
-    for (int i = 1; i <= n - 1; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
-    }
+	cin >> n;
+	for (int i = 1; i <= n - 1; i++)
+	{
+		cin >> a[i] >> b[i];
+		G[a[i]].push_back(b[i]);
+		G[b[i]].push_back(a[i]);
+	}
 
-    // 頂点 1 からの最短距離を求める
-    // maxid1: 頂点 1 から最も離れている(最短距離が長い)頂点
-    getdist(1);
-    int maxn1 = -1, maxid1 = -1;
-    for (int i = 1; i <= n; i++)
-    {
-        if (maxn1 < dist[i])
-        {
-            maxn1 = dist[i];
-            maxid1 = i;
-        }
-    }
-
-    // 頂点 maxid1 からの最短距離を求める
-    // maxn2: 木の直径(最短距離の最大値)
-    getdist(maxid1);
-    int maxn2 = -1;
-    for (int i = 1; i <= n; i++)
-        chmax(maxn2, dist[i]);
-
-    cout << maxn2 + 1 << endl;
-    return 0;
+	getdist(1); // 頂点1からの最短距離を求める
+	int maxi = -1, maxid = -1;
+	for (int i = 1; i <= n; i++)
+	{
+		if (maxi < dist[i])
+		{
+			maxi = dist[i];
+			maxid = i;
+		}
+	}
+	getdist(maxid); // 頂点maxidからの最短距離を求める
+	maxi = -1;
+	for (int i = 1; i <= n; i++)
+	{
+		chmax(maxi, dist[i]);
+	}
+	cout << maxi + 1 << endl;
+	return 0;
 }
