655706d46656add01e472b9948d84e1d031cb638
Ryomanba <datemanba3@gmail.com>
2021-08-23 23:55:11 +0900

    三角関数

--- Problems/018.cpp
+++ Problems/018.cpp
@@ -14,18 +14,18 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
 const double PI = acos(-1);
-double	t, l, x, y, e;
-int	q;
+double	t, l, x, y, e, q;
 
 double solve(double d)
 {
-	double cx, cy, cz, d1, d2;
+	double cx, cy, cz, d1, d2, fukaku;
 	cx = 0; 											// x座標
 	cy = -(l / 2.0) * sin(d / t * 2.0 * PI); 			// y座標
 	cz = (l / 2.0) - (l / 2.0) * cos(d / t * 2.0 * PI); // z座標
 	d1 = sqrt(pow((cx - x),2)+ pow((cy - y), 2));		// 水平距離
 	d2 = cz;											// 観覧車の高さ
-	return atan2(d2, d1) * 180.0L / PI; // atan2(y,x)でラジアンで返す
+	fukaku = atan2(d2, d1);
+	return fukaku * 180.0L / PI; 	// atan2(y,x)でラジアンで返す
 }
 
 int main()
@@ -33,7 +33,7 @@ int main()
 	cin >> t;
 	cin >> l >> x >> y;
 	cin >> q;
-	for (int i = 1; i <= q; i++)
+	rep(i,q)
 	{
 		cin >> e;
 		cout << solve(e) << endl;
